de168c94f31e1eb38233d19db3f3708663b193c0
yopse <82827725+yopse@users.noreply.github.com>
2021-12-06 22:15:44 +0530

    javascript add start using

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
+var btnTranslate = document.querySelector("#translation-btn");
 
+var textInput = document.querySelector("#text-input");
+
+var resetBtn = document.querySelector("#translation-btn-reset");
+
+var outputBox = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json";
+
+function getTranslatorURL(input){
+  return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+}
+
+function clickHandler() {
+  var inputText = textInput.value;
+
+  //server call for processing
+       
+   fetch(getTranslatorURL(inputText))
+   .then(response => response.json()).then(json=>{
+
+      var translatedText = json.content.translated;
+
+      outputBox.innerText = translatedText;
+   }).catch(errorHandler)
+
+};
+
+
+function resetText() {
+
+    outputBox.innerText = "";
+    textInput.value="";
+
+}
+
+
+
+
+
+btnTranslate.addEventListener('click',clickHandler);
+resetBtn.addEventListener('click',resetText);
